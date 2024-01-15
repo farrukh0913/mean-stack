580ce1212bc6567c1b8880c28b23213ecd03b674
Hamza Burhan <hamzaburhan0985@gmail.com>
2024-01-15 20:04:57 +0500

    pricing page implemented

--- mean-front/src/app/pricing-page/pricing-page.component.ts
+++ mean-front/src/app/pricing-page/pricing-page.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-pricing-page',
   templateUrl: './pricing-page.component.html',
   styleUrls: ['./pricing-page.component.scss']
 })
 export class PricingPageComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  ngAfterViewInit(): void {
+    const planBtns: NodeListOf<HTMLButtonElement> = this.el.nativeElement.querySelectorAll(".card-btn-parent button");
+    const plans: NodeListOf<HTMLDivElement> = this.el.nativeElement.querySelectorAll(".card-body > div");
+
+    planBtns.forEach(planBtn => {
+      this.renderer.listen(planBtn, 'click', () => {
+        this.removeClass();
+        planBtn.classList.add("active");
+        let btnVal = planBtn.getAttribute("id");
+        let btnId = "#card-" + btnVal;
+        this.el.nativeElement.querySelector(btnId).classList.add("active");
+      });
+    });
+  }
+
+  private removeClass() {
+    const planBtns: NodeListOf<HTMLButtonElement> = this.el.nativeElement.querySelectorAll(".card-btn-parent button");
+    const plans: NodeListOf<HTMLDivElement> = this.el.nativeElement.querySelectorAll(".card-body > div");
+
+    planBtns.forEach(planBtn => {
+      if (planBtn.classList.contains("active")) {
+        planBtn.classList.remove("active");
+      }
+    });
+
+    plans.forEach(plan => {
+      if (plan.classList.contains("active")) {
+        plan.classList.remove("active");
+      }
+    });
+  }
 
 }
